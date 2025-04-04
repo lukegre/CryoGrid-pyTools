7169ef2840d26c3978e83cea10daa6847fd4961c
Luke <gregorl@ethz.ch>
2025-04-04 12:01:44 +0200

    added data sources and resturctured - aster DEM/emissivity - MODIS albedo - randalf glacier index - country polygons - rock glacier polygons

--- cryogrid_pytools/data/from_planetary_computer.py
+++ cryogrid_pytools/data/from_planetary_computer.py
@@ -1,16 +1,7 @@
 try:
-    import earthaccess as _earthaccess
-    import geopandas as _gpd
-    import planetary_computer as _planetary_computer
-    import pooch as _pooch
-    import pystac_client as _pystac_client
     import rioxarray as _rxr  # noqa
     import stackstac as _stackstac  # noqa
     import xarray_raster_vector as _xrv  # noqa
-    from era5_downloader.defaults import (
-        create_cryogrid_forcing_fetcher as make_era5_downloader,  # noqa
-    )
-    from memoization import cached as _cached
 except ImportError as e:
     missing_package = str(e).split("'")[1]
     raise ImportError(
@@ -22,60 +13,10 @@ except ImportError as e:
 # then warn the user that this package cannot be imported
 import xarray as _xr
 from loguru import logger as _logger
+from . import utils
 
 
-def _decorator_dataarray_to_bbox(func):
-    """
-    A decorator that processes the first argument of the decorated function to handle
-    either an xarray DataArray or a bounding box tuple. If the first argument is a
-    DataArray, it extracts the bounding box from the DataArray and reprojects the
-    output to match the DataArray's projection.
-
-    Parameters
-    ----------
-    func : callable
-        The function to be decorated. The function should accept a
-        bounding box as its first argument.
-
-    Returns
-    -------
-    callable
-        The wrapped function with the additional functionality of handling
-        DataArray and reprojecting the output.
-    """
-
-    @_cached
-    def wrapper(*args, **kwargs):
-        if len(args) >= 1:
-            bbox_or_target = args[0]
-            args = args[1:]
-        else:
-            bbox_or_target = kwargs.pop("bbox", None) or kwargs.pop("bbox_WSEN", None)
-
-        if isinstance(bbox_or_target, _xr.DataArray):
-            da = bbox_or_target
-            bbox = da.rv.get_bbox_latlon()
-            out = func(bbox, *args, **kwargs)
-            out = out.rio.reproject_match(da)
-
-        elif isinstance(bbox_or_target, tuple):
-            bbox = bbox_or_target
-            out = func(bbox_or_target, *args, **kwargs)
-
-        else:
-            message = (
-                f"The first argument must be a bounding box tuple or an xarray DataArray, "
-                f"but got {type(bbox_or_target)} instead."
-            )
-
-            raise ValueError(message)
-
-        return out
-
-    return wrapper
-
-
-@_decorator_dataarray_to_bbox
+@utils._decorator_dataarray_to_bbox
 def get_dem_copernicus30(
     bbox_WSEN: list, res_m: int = 30, epsg=32643, smoothing_iters=2, smoothing_size=3
 ) -> _xr.DataArray:
@@ -102,14 +43,15 @@ def get_dem_copernicus30(
     xarray.DataArray
         The DEM data as an xarray DataArray with attributes.
     """
-    from .utils import drop_coords_without_dim
+    from ..utils import drop_coords_without_dim
+    from .utils import check_epsg, search_stac_items_planetary_computer, smooth_data
 
     check_epsg(epsg)
 
     assert res_m >= 30, (
         "The resolution must be greater than 30m for the COP DEM Global 30m dataset."
     )
-    res = res_m / 111111 if epsg == 4326 else res_m
+    res = res_m / 111_111 if epsg == 4326 else res_m
 
     _logger.info("Fetching COP DEM Global 30m data from Planetary Computer")
     items = search_stac_items_planetary_computer("cop-dem-glo-30", bbox_WSEN)
@@ -132,7 +74,7 @@ def get_dem_copernicus30(
     return da_dem
 
 
-@_decorator_dataarray_to_bbox
+@utils._decorator_dataarray_to_bbox
 def get_esa_land_cover(bbox_WSEN: tuple, res_m: int = 30, epsg=32643) -> _xr.DataArray:
     """
     Get the ESA World Cover dataset on the target grid and resolution.
@@ -152,6 +94,11 @@ def get_esa_land_cover(bbox_WSEN: tuple, res_m: int = 30, epsg=32643) -> _xr.Dat
         A DataArray with the land cover data on the target grid. Contains
         attributes 'class_values', 'class_descriptions', 'class_colors' for plotting.
     """
+    from .utils import (
+        check_epsg,
+        get_res_in_proj_units,
+        search_stac_items_planetary_computer,
+    )
 
     def get_land_cover_classes(item):
         """
@@ -211,7 +158,7 @@ def get_esa_land_cover(bbox_WSEN: tuple, res_m: int = 30, epsg=32643) -> _xr.Dat
     return da
 
 
-@_decorator_dataarray_to_bbox
+@utils._decorator_dataarray_to_bbox
 def get_sentinel2_data(
     bbox_WSEN: tuple,
     years=range(2018, 2025),
@@ -243,7 +190,12 @@ def get_sentinel2_data(
     xr.DataArray
         DataArray containing the fetched Sentinel-2 data.
     """
-    from .utils import drop_coords_without_dim
+    from ..utils import drop_coords_without_dim
+    from .utils import (
+        check_epsg,
+        get_res_in_proj_units,
+        search_stac_items_planetary_computer,
+    )
 
     check_epsg(epsg)
     res = get_res_in_proj_units(res_m, epsg, min_res=10)
@@ -289,7 +241,7 @@ def get_sentinel2_data(
     return da
 
 
-@_decorator_dataarray_to_bbox
+@utils._decorator_dataarray_to_bbox
 def get_snow_melt_doy(
     bbox_WSEN: tuple, years=range(2018, 2025), res_m: int = 30, epsg=32643
 ) -> _xr.DataArray:
@@ -318,98 +270,12 @@ def get_snow_melt_doy(
     )
 
     _logger.info("Calculating snow melt day of year (DOY) from Sentinel-2 SCL data")
-    doy = da.groupby("time.year").apply(calc_sentinel2_snow_melt_doy)
+    doy = da.groupby("time.year").apply(_calc_sentinel2_snow_melt_doy)
 
     return doy
 
 
-@_cached
-def get_randolph_glacier_inventory(target_dem=None, dest_dir=None):
-    """
-    Fetches the Randolph Glacier Inventory (RGI) data and returns it as a GeoDataFrame or raster dataset.
-
-    Parameters
-    ----------
-    target_dem : optional
-        A digital elevation model (DEM) object. If provided, the function will return
-        the RGI data clipped to the bounding box of the DEM and reprojected to the DEM's CRS.
-    dest_dir : str, optional
-        The directory where the downloaded RGI data will be stored. If None, the data will
-        be stored in the pooch cache directory (~/.cache/pooch/).
-
-    Returns
-    -------
-    GeoDataFrame or raster dataset
-        If target_dem is None, returns a GeoDataFrame containing the RGI data.
-        If target_dem is provided, returns a raster dataset clipped and reprojected to the DEM.
-    """
-
-    url = "https://daacdata.apps.nsidc.org/pub/DATASETS/nsidc0770_rgi_v7/regional_files/RGI2000-v7.0-G/RGI2000-v7.0-G-13_central_asia.zip"
-
-    downloader = _pooch.HTTPDownloader(
-        progressbar=True, headers=get_earthaccess_session().headers
-    )
-    flist = download_url(url, path=dest_dir, downloader=downloader)
-
-    fname_shp = [f for f in flist if f.endswith(".shp")][0]
-
-    _logger.log(
-        "INFO",
-        "RGI: Fetching Randolph Glacier Inventory - see https://www.glims.org/rgi_user_guide/welcome.html",
-    )
-    _logger.log("DEBUG", f"RGI: URL = {url}")
-    _logger.log("DEBUG", f"RGI: FILE = {fname_shp}")
-
-    if target_dem is None:
-        # reads the whole file
-        df = _gpd.read_file(fname_shp)
-    else:
-        # gets the bounding box and then reads the file
-        bbox = target_dem.rv.get_bbox_latlon()
-        df = _gpd.read_file(fname_shp, bbox=bbox).to_crs(target_dem.rio.crs)
-        df = df.dissolve()
-        ds = df.rv.to_raster(target_dem)
-        return ds
-
-    return df
-
-
-def smooth_data(da: _xr.DataArray, kernel_size: int = 3, n_iters=1) -> _xr.DataArray:
-    """
-    Smooth the data using a rolling mean filter (box kernel).
-
-    Parameters
-    ----------
-    da : xarray.DataArray
-        The input data as an xarray DataArray.
-    kernel_size : int
-        The size of the kernel for the rolling mean filter.
-    n_iters : int
-        The number of iterations to apply the filter.
-
-    Returns
-    -------
-    xarray.DataArray
-        The smoothed data as an xarray DataArray.
-    """
-
-    da_smooth = da.copy()
-    for _ in range(n_iters):
-        da_smooth = da_smooth.rolling(
-            x=kernel_size, y=kernel_size, center=True, min_periods=1
-        ).mean()
-
-    if n_iters:
-        da_smooth = da_smooth.assign_attrs(
-            smoothing_kernel="box_kernel",
-            smoothing_kernel_size=kernel_size,
-            smoothing_iterations=n_iters,
-        )
-
-    return da_smooth
-
-
-def calc_sentinel2_snow_melt_doy(da_scl) -> _xr.DataArray:
+def _calc_sentinel2_snow_melt_doy(da_scl) -> _xr.DataArray:
     """
     Calculates the day of year (DOY) when snow melt occurs based on Sentinel-2 SCL data.
 
@@ -516,155 +382,3 @@ def calc_sentinel2_snow_melt_doy(da_scl) -> _xr.DataArray:
     snow_melt_day = get_max_day_of_year_from_mask(snow_mask)
 
     return snow_melt_day
-
-
-def get_earthaccess_session():
-    """
-    Logs into earthaccess and gets session info.
-
-    Returns
-    -------
-    session
-        The session information.
-    """
-
-    auth = _earthaccess.login(persist=True)
-    session = auth.get_session()
-
-    return session
-
-
-def download_url(url, **kwargs):
-    """
-    Download a file from a given URL and process it if necessary.
-
-    Parameters
-    ----------
-    url : str
-        The URL of the file to download.
-    **kwargs : Additional properties passed to pooch.retrieve.
-        These properties will override the default settings.
-
-    Returns
-    -------
-    list
-        A list of file paths to the downloaded (and possibly
-        decompressed) files.
-    """
-
-    if url.endswith(".zip"):
-        processor = _pooch.Unzip()
-    elif url.endswith(".tar.gz"):
-        processor = _pooch.Untar()
-    else:
-        processor = None
-
-    default_props = dict(
-        known_hash=None,
-        fname=url.split("/")[-1],
-        path=None,
-        downloader=_pooch.HTTPDownloader(progressbar=True),
-        processor=processor,
-    )
-
-    props = default_props | kwargs
-    flist = _pooch.retrieve(url=url, **props)
-
-    return flist
-
-
-def search_stac_items_planetary_computer(collection, bbox, **kwargs) -> list:
-    """
-    Searches for STAC items from the Planetary Computer.
-
-    Parameters
-    ----------
-    collection : str
-        The name of the collection to search within.
-    bbox : list
-        The bounding box to search within, specified as [minx, miny, maxx, maxy].
-    **kwargs : Additional keyword arguments to pass to the search.
-
-    Returns
-    -------
-    list
-        A list of STAC items matching the search criteria.
-    """
-
-    URL_PLANETARY_COMPUTER = "https://planetarycomputer.microsoft.com/api/stac/v1"
-
-    catalog = _pystac_client.Client.open(
-        url=URL_PLANETARY_COMPUTER, modifier=_planetary_computer.sign_inplace
-    )
-
-    search = catalog.search(collections=[collection], bbox=bbox, **kwargs)
-
-    items = search.item_collection()
-
-    return items
-
-
-def check_epsg(epsg: int) -> bool:
-    """
-    Check if the provided EPSG code is valid.
-
-    Parameters
-    ----------
-    epsg : int
-        The EPSG code to be checked.
-
-    Returns
-    -------
-    bool
-        True if the EPSG code is valid, False otherwise.
-
-    Raises
-    ------
-    AssertionError
-        If the EPSG code is not valid.
-    """
-
-    def check_epsg(epsg: int) -> bool:
-        """
-        Check if the provided EPSG code is valid.
-
-        This function checks whether the given EPSG code is either a UTM code
-        (starting with '326') or the Lat/Lon code (4326).
-
-        Args:
-            epsg (int): The EPSG code to be checked.
-
-        Returns:
-            bool: True if the EPSG code is valid, False otherwise.
-
-        Raises:
-            AssertionError: If the EPSG code is not valid.
-        """
-
-    is_valid_epsg = str(epsg).startswith("326") or (epsg == 4326)
-    assert is_valid_epsg, "The EPSG code must be UTM (326xx) or Lat/Lon (4326)."
-
-
-def get_res_in_proj_units(res_m, epsg, min_res=30):
-    """
-    Check if the resolution is valid for the given EPSG code.
-
-    Parameters
-    ----------
-    res_m : int
-        The resolution in meters.
-    epsg : int
-        The EPSG code of the projection.
-    min_res : int
-        The minimum resolution required for the dataset.
-
-    Returns
-    -------
-    res : int
-        The resolution in the units of the projection.
-    """
-    message = f"The resolution must be greater than {min_res}m for this collection"
-    assert res_m > min_res, message
-    res = res_m / 111111 if epsg == 4326 else res_m
-
-    return res
